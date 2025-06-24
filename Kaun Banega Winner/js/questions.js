cf13e0be28cffcb52bff165133c52bb8ed055f1c
Imran S. <128597254+ImranS17@users.noreply.github.com>
2025-06-24 11:35:27 +0530

    Files Upload

---
+++ Kaun Banega Winner/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many eyes does a bee have?",
+    answer: "5",
+    options: [
+      "4",
+      "5",
+      "8",
+      "2"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many flowers does it take to make Truly Raw Honey?",
+    answer: "4 Million",
+    options: [
+      "2000",
+      "4 Million",
+      "2 Million",
+      "40"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How much truly raw honey does a honeybee produce in a lifetime?",
+    answer: "1 tablespoon",
+    options: [
+      "2tsp",
+      "1 tablespoon",
+      "1/12 tsp",
+      "1 cup"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How fast does a honeybee fly?",
+    answer: "15mph",
+    options: [
+      "5mph",
+      "15mph",
+      "18mph",
+      "20mph"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How can you identify Truly Raw Honey?",
+    answer: "It's Translucent",
+    options: [
+      "It's Transparent",
+      "It's Translucent",
+      "It's opaque",
+      "Don't know"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How much truly raw honey would it take to fuel a bee's flight around the world?",
+    answer: "1 ounce",
+    options: [
+      "1 ounce",
+      "1 pound",
+      "1 litre",
+      "2kgs"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Truly Raw Honey goes through which process?",
+    answer: "Cold Process",
+    options: [
+      "Cold Process",
+      "Fermentation process",
+      "Heat Process",
+      "Grinding process"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many pairs of wings does a bee have?",
+    answer: "2",
+    options: [
+      "2",
+      "1",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What do bees collect from flowers?",
+    answer: "Nectar & Pollen",
+    options: [
+      "Honey",
+      "Nectar & Pollen",
+      "Petals",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Honeybees communicated with each other by which type of dance?",
+    answer: "The waggle dance",
+    options: [
+      "The squiggle dance",
+      "The waggle dance",
+      "The wiggle dance",
+      "The Giggle dance"
+    ]
+  },
+];
